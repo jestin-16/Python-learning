95b1de1b2d136280a248abd9d6698cd3efd16bbf
jestin16 <jestinshaji777@gmail.com>
2025-09-20 22:07:23 +0530

    intial push

---
+++ Helloworld/app.py
@@ -0,0 +1,19 @@
+# print("Jestin Shaji")
+#
+# print('o----')
+# print(' ||||')
+#
+# print('*' * 10)
+
+# price = 10
+# price = 20
+# name='jestin'
+# is_published = False
+# print(price)
+
+# name = 'john smith'
+# age = 20
+# is_new = True
+
+name = input("Enter your name: ")
+print("Hello, "+ name)
