81af28816040b056f24dd54c3abbb8c9a1afbddc
jestin16 <jestinshaji777@gmail.com>
2025-09-21 17:47:15 +0530

    update in app.py

--- Helloworld/app.py
+++ Helloworld/app.py
@@ -23,6 +23,31 @@
 # print(type(birth_year))
 # age =2025-int(birth_year)
 # print(age)
-weight=input("enter your weight")
-weight_kilogram= int(weight)*0.45
-print(weight_kilogram)
+# weight=input("enter your weight")
+# weight_kilogram= int(weight)*0.45
+# print(weight_kilogram)
+
+#
+# course = '''python's for beginners'''
+# another = course[:]
+# print(another)
+# print(course[:])
+
+# name='Jennifer'
+# print(name[1:-1])
+
+#
+# first = 'john'
+# last = 'doe'
+# print(f"{first} [{last}] is a coder")
+
+# course = 'python for beginners'
+# # print(len(course))
+# # print(course.upper())
+# # print(course)
+# # print(course.lower())
+# print(course.find('python'))
+#
+# print('p' in course)
+
+
