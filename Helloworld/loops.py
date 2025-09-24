4c93cedc9813919bcc938f549b5dcbe7c02bdb4d
jestin16 <jestinshaji777@gmail.com>
2025-09-24 20:14:23 +0530

    update in loops.py

--- Helloworld/loops.py
+++ Helloworld/loops.py
@@ -1,4 +1,30 @@
-i=1
-while i<=5:
-    print(i)
-    i=i+1
+# i=1
+# while i<=5:
+#     print(i)
+#     i=i+1
+# sum=0
+# for i in range(10):
+#     sum=sum+i
+# print(sum)
+
+# i=1
+# while i<=5:
+#     print("*" * i)
+#     i=i+1
+
+# num=9
+# i=0
+# while i<3:
+#     n=int(input("Guess: "))
+#     i=i+1
+#     if n==num:
+#         print("Correct!")
+#         break
+#     else:
+#         print("Incorrect!")
+# else:
+#     print("your chances over")
+
+
+i=0
+while
